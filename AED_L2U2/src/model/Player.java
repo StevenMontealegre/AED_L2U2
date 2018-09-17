8d068f17d484dc17a44dbb6955fa617c7d1a46e2
Steven Montealegre <Steven Montealegre@LAPTOP-6U069POD>
2018-09-16 21:18:56 -0500

    changes in the model

--- AED_L2U2/src/model/Player.java
+++ AED_L2U2/src/model/Player.java
@@ -2,4 +2,49 @@ package model;
 
 public class Player {
 
+	String nickname, ip;
+	int life;
+	Player siguiente;
+
+	public Player(String nickname, String ip, int life) {
+		super();
+		this.nickname = nickname;
+		this.ip = ip;
+		this.life = life;
+		siguiente = null;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public Player getSiguiente() {
+		return siguiente;
+	}
+
+	public void setSiguiente(Player siguiente) {
+		this.siguiente = siguiente;
+	}
+
 }
